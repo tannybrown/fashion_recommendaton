de7e6c78a2afbf27d7938af47b751a39c16287cf
tannybrown <121401159+tannybrown@users.noreply.github.com>
2023-10-21 21:00:21 +0900

    Add files via upload

---
+++ recommender_server.py
@@ -0,0 +1,188 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from typing import Optional
+# import recommender
+# 연결하기. 넣기.
+
+app = FastAPI()
+
+class WeatherUltraShort(BaseModel):
+    fcstDate: int
+    fcstTime: int
+    temp: int
+    rainAmount: int
+    windSpeed: int
+    humid: int
+    sky: int
+
+
+class WeatherShort(BaseModel):
+    fcstDate: int
+    fcstTime: int
+    temp: int
+    rainAmount: int
+    windSpeed: int
+    humid: int
+    sky: int
+    rainPercentage: int
+
+class WeatherMid(BaseModel):
+    fcstDate: int
+    rainPercentageAm: int
+    rainPercentagePm: int
+    skyAm: int
+    skyPm: int
+    tempLowest: int
+    tempHighest: int
+class WeatherAP(BaseModel) :
+    fcstDate: int
+    pm10Value : int
+    pm25Value : int
+
+class WeatherResponse(BaseModel):
+    weatherUltraShort: List[WeatherUltraShort]
+    weatherShort: List[WeatherShort]
+    weatherMid: List[WeatherMid]
+    weatherAP: List[WeatherAP]
+
+class ResponseData(BaseModel):
+    weatherResponse: WeatherResponse
+    version: float
+    message: str
+
+
+# uvicorn main:app --reload
+@app.post("/parse_address_data")
+def parse_address_data(responseData: ResponseData):
+    # 추천 id list
+    return_arr = []
+
+    # data 쪼개기
+
+    # 추천할 날짜(recomDate), 0은 오늘 1은 내일
+    tomorrow = 0
+    targetWeather = []
+    weatherResponse = responseData.weatherResponse
+    weatherUltraShort = weatherResponse.weatherUltraShort
+    weatherShort = weatherResponse.weatherShort
+
+    curTime = weatherUltraShort[0].fcstTime
+    targetDate = weatherUltraShort[0].fcstDate
+    # 7시 이후면 내일 날씨를 추천 해주는 것
+    if curTime >= 2200:
+        tomorrow = 1
+        targetDate = weatherShort[0].fcstDate
+
+    #내일 날씨 추천
+    if tomorrow :
+        # 다음날 예보 수집하기
+        # 초단기 부터 수집하기
+        index = 0
+        for i in range(1,len(weatherUltraShort)):
+            if weatherUltraShort[i].fcstTime == 0 :
+                index = i
+                break
+        if index == 0 :
+            targetWeather = weatherUltraShort[index:].copy()
+
+        # 여기부터 단기 예보에서 뽑아내기
+        # targetWeather에 날씨 정보 입력하기
+        for i in range(len(weatherShort)):
+            # targetWeather가 비었으면
+            if targetWeather == [] :
+                targetWeather.append(weatherShort[i])
+            # 비어있지 않다면, 겹치지 않게 fcstTime이 더 크면 넣기
+            else :
+                if targetWeather[-1].fcstTime < weatherShort[i].fcstTime:
+                    targetWeather.append(weatherShort[i])
+                else :
+                    # 다음날로 넘어갔는지 확인하기, 넘어갔으면 break
+                    if targetDate != weatherShort[i].fcstDate:
+                        break
+
+
+
+
+
+    # 오늘 날씨 추천
+    else :
+        # 현재부터 오늘의 마지막 날씨 까지 받기
+        index = 0
+        for i in range(1,len(weatherUltraShort)):
+            if weatherUltraShort[i].fcstTime == 0:
+                index = i
+                break
+
+        if index == 0 :
+            # index가 0이면 찾지 못한 것이므로, 그냥 전부 넣으면 됌
+            targetWeather = weatherUltraShort.copy()
+        else :
+            targetWeather = weatherUltraShort[:index]
+
+        #단기 예보에서 정보얻기
+        for i in range(len(weatherShort)):
+            if targetWeather[-1].fcstTime < weatherShort[i].fcstTime:
+                targetWeather.append(weatherShort[i])
+            else :
+                # 둘이 같으면 겹쳐진거고, 다르면 다른날인것. 즉 날짜가 넘어간거임
+                if targetDate != weatherShort[i].fcstDate:
+                    break
+
+
+
+
+        # targetWeather를 파싱해서 날씨, 습도, 등등 파악하기
+        # 알고리즘에 집어넣기.
+
+    tmp = []
+    humid = []
+    rain = []
+    wind = []
+
+    # 시간에 따라서 분류, 밤 ~ 아침(22~10), 점심(10~17), 그리고 저녁(17~22) 으로 3개의 분류.
+    if curTime >= 2200 or curTime < 1000 :
+        for weather in targetWeather :
+            # 9시 이전은 고려 안함
+            if weather.fcstTime < 900 :
+                continue
+            else :
+                if weather.fcstTime > 1500 :
+                    break
+                else :
+                    tmp.append(weather.temp * 0.1)
+                    humid.append(weather.humid)
+                    rain.append(weather.rainAmount)
+                    wind.append(weather.windSpeed)
+
+    elif curTime >= 1000 and curTime < 1700 :
+        # 18시까지 보기
+        for weather in targetWeather :
+            if weather.fcstTime > 1800 :
+                break
+            else :
+                tmp.append(weather.temp * 0.1)
+                humid.append(weather.humid)
+                rain.append(weather.rainAmount)
+                wind.append(weather.windSpeed)
+
+    # 저녁날씨 옷 추천임.
+    else :
+        for weather in targetWeather :
+            tmp.append(weather.temp * 0.1)
+            humid.append(weather.humid)
+            rain.append(weather.rainAmount)
+            wind.append(weather.windSpeed)
+
+    # 일단 1안으로 평균 내서 그냥 그값으로 가기.
+
+    avg = [sum(tmp)/len(tmp),sum(humid)/len(humid),sum(rain)/len(rain),sum(wind)/len(wind)]
+
+
+
+
+
+
+    return avg
+
+    # return targetWeather
